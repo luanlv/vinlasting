eb611c778aecb8f711816d599ab21fe2d2798469
Lưu Văn Luận <luanlv@users.noreply.github.com>
2018-07-24 13:29:27 +0700

    init

---
+++ pages/_document.js
@@ -0,0 +1,105 @@
+// _document is only rendered on the server side and not on the client side
+// Event handlers like onClick can't be added to this file
+
+// ./pages/_document.js
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <style>{`
+            img.wp-smiley,
+            img.emoji {
+              display: inline !important;
+              border: none !important;
+              box-shadow: none !important;
+              height: 1em !important;
+              width: 1em !important;
+              margin: 0 .07em !important;
+              vertical-align: -0.1em !important;
+              background: none !important;
+              padding: 0 !important;
+            }
+          `}</style>
+          <link rel="stylesheet" href="http://www.hoaphat.com.vn/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=4.4"/>
+          <link rel="stylesheet" href="http://www.hoaphat.com.vn/wp-content/plugins/woocommerce/assets/css/woocommerce-layout.css?ver=2.5.2"/>
+          <link rel="stylesheet" href="http://www.hoaphat.com.vn/wp-content/plugins/woocommerce/assets/css/woocommerce-smallscreen.css?ver=2.5.2" media='only screen and (max-width: 768px)'/>
+          <link rel="stylesheet" href="http://www.hoaphat.com.vn/wp-content/plugins/woocommerce/assets/css/woocommerce.css?ver=2.5.2"/>
+          <link rel="stylesheet" href="http://www.hoaphat.com.vn/wp-content/themes/tapdoan/css/bootstrap.min.css?ver=1.0"/>
+          <link rel="stylesheet" href="http://www.hoaphat.com.vn/wp-content/themes/tapdoan/css/bxslider.css?ver=1.0"/>
+          <link rel="stylesheet" href="http://www.hoaphat.com.vn/wp-content/themes/tapdoan/css/font-awesome.min.css?ver=1.0" />
+          <link rel="stylesheet" href="http://www.hoaphat.com.vn/wp-content/themes/tapdoan/css/gallery.css?ver=1.0" />
+          
+        
+          <script type='text/javascript' src='http://www.hoaphat.com.vn/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+          <script type='text/javascript' src='http://www.hoaphat.com.vn/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+          <script type='text/javascript' src='http://www.hoaphat.com.vn/wp-content/themes/tapdoan/js/jquery.min.js?ver=1.0'></script>
+
+          <script src="http://www.hoaphat.com.vn/wp-content/themes/tapdoan/js/bootstrap.min.js?ver=1.0" />
+          <script src="http://www.hoaphat.com.vn/wp-content/themes/tapdoan/js/jquery.bxslider.min.js?ver=1.0" />
+          <script src="http://www.hoaphat.com.vn/wp-content/themes/tapdoan/js/highslide-with-gallery.js?ver=1.0" />
+          <script src="http://www.hoaphat.com.vn/wp-content/themes/tapdoan/js/highslide-with-gallery.js?ver=1.0" />
+        
+          <style>{`
+          .g { margin:0px; padding:0px; overflow:hidden; line-height:1; zoom:1; }
+          .g img { height:auto; }
+          .g-col { position:relative; float:left; }
+          .g-col:first-child { margin-left: 0; }
+          .g-col:last-child { margin-right: 0; }
+          .g-2 { min-width:0px; max-width:625px; }
+          .b-2 { margin:0px; }
+          .g-3 { min-width:0px; max-width:625px; }
+          .b-3 { margin:0px; }
+          @media only screen and (max-width: 480px) {
+            .g-col, .g-dyn, .g-single { width:100%; margin-left:0; margin-right:0; }
+          }
+          .bx-pager {
+            display: none;
+          }
+          
+          .logo {
+            left: 0 !important;
+          }
+
+          `}</style>
+          <link rel="stylesheet" href="/static/style.css"/>
+          <script dangerouslySetInnerHTML={{
+            __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/5b503710df040c3e9e0bba7e/default';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+            `
+          }} />
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+          <script dangerouslySetInnerHTML={
+            {__html: ``}
+            } />
+
+          <style>{`
+            @media (max-width: 600px){
+              .navbar-brand {
+                height: 20px !important;
+              }
+            }
+          `}</style>
+        </body>
+      </html>
+    )
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,249 @@
+
+
+class ReactWr extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="fix" />
+                
+                <div className="box_header">
+                    <div className="box_fixed">
+                        <div className="container">
+                        <div className="menu_fixed">
+                            <div id="polylang-2" className="widget widget_polylang"><ul>
+                                <li className="lang-item lang-item-2 lang-item-vi current-lang"><a hrefLang="vi" href="#"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAALCAIAAAD5gJpuAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAFsSURBVHjaYvzPgAD/UNlYEUAAmuTYAAAQhAEYqF/zFbe50RZ1cMmS9TLi0pJLRjZohAMTGFUN9HdnHgEE1sDw//+Tp0ClINW/f4NI9d////3+f+b3/1+////+9f/XL6A4o6ws0AaAAGIBm/0fRTVQ2v3Pf97f/4/9Aqv+DdHA8Ps3UANAALEAMSNQNdDGP3+ALvnf8vv/t9//9X/////7f+uv/4K//iciNABNBwggsJP+/IW4kuH3n//1v/8v+wVSDURmv/57//7/CeokoKFA0wECiAnkpL9/wH4CO+DNr/+VQA1A9PN/w6//j36CVIMRxEkAAQR20m+QpSBXgU0CuSTj9/93v/8v//V/xW+48UBD/zAwAAQQSAMzOMiABoBUswCd8ev/M7A669//OX7///Lr/x+gBlCoAJ0DEEAgDUy//zBISoKNAfoepJNRFmQkyJecfxj4/kDCEIiAigECiPErakTiiWMIAAgwAB4ZUlqMMhQQAAAAAElFTkSuQmCC" title="Tiếng Việt" alt="Tiếng Việt" /></a></li>
+                            </ul>
+                            </div>							
+                        </div>
+                        </div>
+                    </div>
+                    <div className="box_menuMain" id="box_menuMain">
+                        <div className="container">
+                        <div className="row">
+                            <div className="hidden-sm hidden-xs col-md-2 images">
+                            <a href="/"><img className="logo" alt src="/static/logo.jpg" /></a>
+                            </div>
+                            <div className="col-md-10 col-sm-12 full">
+                            <nav className="navbar navbar-default" role="navigation" id="navigation">
+                                <div className="box_content">
+                                <div className="navbar-header hidden-lg hidden-md">
+                                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#my-navbar">
+                                    <span className="sr-only">Toggle Menu</span>
+                                    <span className="icon-bar" />
+                                    <span className="icon-bar" />
+                                    <span className="icon-bar" />
+                                    </button>
+                                    <a className="navbar-brand" href="/"><img style={{left: 60}} src="/static/logo.jpg" /></a>
+                                    <a className="navbar-brand search">
+                                    <div className="dropdown">
+                                        <ul className="dropdown-menu search_drop" aria-labelledby="dropdownMenu1">
+                                        <li>
+                                            <form role="search" action="#/" method="get" className="search-form" id="searchform">
+                                            <div className="input-group input-group-sm">
+                                                <input type="text" name="s" id="s" className="form-control" placeholder="Search for..." onfocus="if (this.value == 'Search') {this.value = '';}" onblur="if (this.value == '')  {this.value = '';}" defaultValue />
+                                                <span className="input-group-btn">
+                                                <button className="btn btn-default search-submit" id="searchsubmit" type="submit">
+                                                    <span className="glyphicon glyphicon-search" />
+                                                </button>
+                                                </span>
+                                            </div>
+                                            </form>
+                                        </li>
+                                        </ul>
+                                    </div>
+                                    </a>
+                                </div>
+                                <div className="collapse navbar-collapse" id="my-navbar">
+                                    <ul className="nav navbar-nav navbar-right hidden-xs">
+                                    <li className="dropdown search">
+                                        <ul className="dropdown-menu">
+                                        <li>
+                                            <form role="search" action="http://www.hoaphat.com.vn/vi/" method="get" className="search-form" id="searchform">
+                                            <div className="input-group input-group-sm">
+                                                <input type="text" name="s" id="s" className="form-control" placeholder="Search for..." onfocus="if (this.value == 'Search') {this.value = '';}" onblur="if (this.value == '')  {this.value = 'Search';}" defaultValue="Search" />
+                                                <span className="input-group-btn">
+                                                <button className="btn btn-default search-submit" id="searchsubmit" type="submit">
+                                                    <span className="glyphicon glyphicon-search" />
+                                                </button>
+                                                </span>
+                                            </div>
+                                            </form>
+                                        </li>
+                                        </ul>
+                                    </li>
+                                    </ul>
+                                    <ul id="menu-main-menu" className><li id="menu-item-52" className="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-52"><a href="index.html">Trang chủ</a></li>
+                                    <li id="menu-item-2332" className="Gioithieu menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-2332"><a href="#">Giới thiệu</a>
+                                        <ul className="sub-menu">
+                                        <li id="menu-item-85" className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-85"><a href="#">Giới thiệu chung</a></li>
+                                        <li id="menu-item-86" className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-86"><a href="#">Sứ mệnh tầm nhìn</a></li>
+                                        </ul>
+                                    </li>
+                                    <li id="menu-item-741" className="Gioithieu menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-741"><a href="#">Lĩnh vực hoạt động</a>
+                                        <ul className="sub-menu">
+                                        <li id="menu-item-742" className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-742"><a href="#l">In ấn</a></li>
+                                        </ul>
+                                    </li>
+                                    <li id="menu-item-8006" className="menu-item menu-item-type-taxonomy menu-item-object-news-category menu-item-8006"><a href="#">Tin tức</a>
+                                    </li>
+                                    </ul>							</div>
+                                </div>
+                            </nav>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+
+
+
+                <div className="show" style={{marginTop: 80}}>
+                <div className="box_slider" id="box_slider">
+                <ul className="bxslider">
+                    <li><a href="#"><img src="/static/slider/1.jpg" /></a></li>
+                    <li><a href="#"><img src="/static/slider/2.jpg" /></a></li>
+                    <li><a href="#"><img src="/static/slider/3.jpg" /></a></li>
+                    <li><a href="#"><img src="/static/slider/4.jpg" /></a></li>
+                    <li><a href="#"><img src="/static/slider/5.jpg" /></a></li>
+                    <li><a href="#"><img src="/static/slider/6.jpg" /></a></li>
+                    <li><a href="#"><img src="/static/slider/7.jpg" /></a></li>
+                    <li><a href="#"><img src="/static/slider/8.jpg" /></a></li>
+                </ul>
+                </div>
+                <style type="text/css" dangerouslySetInnerHTML={{__html: "\n    .show{ position: relative; }\n    .video-home{ position: absolute;z-index: 111;right: 150px;top:150px;}\n" }} />
+                </div>
+
+                <div className="box_slideMember" id="box_slideMember">
+                <div className="container">
+                <div className="bxslider">
+                    <div className="slide"><a href="#" target="_blank">
+                        <img src="http://www.hoaphat.com.vn/wp-content/uploads/lo-go-thep-xay-dung.png" />In ấn</a></div>
+                    <div className="slide"><a href="#" target="_blank"><img src="http://www.hoaphat.com.vn/wp-content/uploads/lo-go-ong-thep.png" />Design</a></div>
+                    <div className="slide"><a href="#" target="_blank"><img src="http://www.hoaphat.com.vn/wp-content/uploads/lo-go-dien-lanh.png" />Website</a></div>
+                    <div className="slide"><a href="#" target="_blank"><img src="http://www.hoaphat.com.vn/wp-content/uploads/lo-go-thiet-bi-phu-tung.png" />Marketing</a></div>
+                </div>
+                </div>
+                </div>
+
+                <div className="box_footer">
+                <div className="container">
+                <div className="row">
+                    <div className="col-xs-12">
+                    <div className="menuBottom">
+                        <ul id="menu-bottom-menu" className><li id="menu-item-104" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-104">
+                            <a href="#">Trang chủ</a></li>
+                        <li id="menu-item-2331" className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-2331"><a href="#">Giới thiệu</a></li>
+                        <li id="menu-item-108" className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-108"><a href="#">Tin tức</a></li>
+                        <li id="menu-item-109" className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-109"><a href="#">Tuyển dụng</a></li>
+                        </ul>	         				<div className="hidden-sm link">
+                        <select onchange="if (this.value != '') window.open(this.value, '_blank');">
+                            <option selected="selected" value>Liên kết Website</option>
+                        </select>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="col-xs-12">
+                    <div className="box_address">
+                        <div id="wp_editor_widget-3" className="widget WP_Editor_Widget"><div className="row">
+                            <div className="col-sm-4 space">
+                            <h3 className="title">Miền Bắc</h3>
+                            <p className="thunnghiem">Văn phòng: Số 51 C1b, Ngõ 231 Tân Mai, Hoàng Mai, Hà Nội</p>
+                            <p>Hotline: 0936.734.686 0989.899.179</p>
+                            </div>
+                        </div>
+                        </div>       				 	</div>
+                    </div>
+                    <div className="col-xs-12">
+                    <div className="design">
+                        <div className="row">
+                        <div className="col-sm-9 col-xs-12">
+                            <div className="headquarter">
+                            <div id="wp_editor_widget-2" className="widget WP_Editor_Widget"><p>2018 ©&nbsp;<strong>Vinlasting</strong>. All Right Reserved.</p>
+                                <p>Trụ sở: Số 51 C1b, Ngõ 231 Tân Mai, Hoàng Mai, Hà Nội, Việt Nam.</p>
+                            </div>	         				 		</div>
+                        </div>
+                        <div className="col-sm-3 col-xs-12">
+                            <div className="designer">
+                            Thiết kế bởi <a href="http://vinlasting.com" target="_blank">Vinlasting</a> 
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                </div>
+
+
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        console.log(window)
+        if(process.browser && typeof window !== 'undefined' &&  typeof $ !== 'undefined' ) {
+            var menu = '#my-navbar > ul:last';
+        $(menu).addClass('nav navbar-nav navbar-right menu');
+        $(menu + ' li').each(function(index) {
+            var chil = $('ul:first', this).length;
+            if(chil > 0) {
+                $(this).addClass('dropdown');
+                $('ul:first', this).addClass('dropdown-menu main').attr('role', 'menu');			
+            }
+        });
+        $('document').ready(function() {
+            $('#my-navbar > ul').show();
+        });
+
+        $(window).scroll(function () {
+            var topPage = $(window).scrollTop();
+            if(topPage > 5) {
+                $('.box_fixed').hide();
+                $('.box_header').css({ 'box-shadow': '0 0 3px #CCC', 'background-color': '#fff'});
+                $('.box_menuMain').css({ 'height': '50px','line-height': '50px', 'background-color': '#fff'});
+                $('.box_menuMain .images img').css({ 'top': '-5px', 'height':'40px'});
+                $('.box_menuMain .navbar-nav > li > a').css({ 'line-height': '50px', 'height':'50px'});
+                
+                $('.box_menuMain').css({ 'height': '50px'});
+                $('.box_menuMain .dropdown-menu').css({ 'background-color': '#fff'});
+            } else {         
+                $('.box_fixed').removeAttr('style');
+                $('.box_header').removeAttr('style'); 
+                $('.box_menuMain .images img').removeAttr('style'); 
+                $('.box_menuMain .navbar-nav > li > a').removeAttr('style'); 
+                $('.box_menuMain').removeAttr('style');  
+                $('.box_menuMain .dropdown-menu').removeAttr('style');
+            }
+
+            $('.navbar-collapse a').click(function () {
+                $('.navbar-collapse').removeClass('in');
+            });
+        });
+
+        $('.box_menuMain .navbar-nav .Gioithieu a').addClass('icon');
+        
+        // Slider
+        $('#box_slideMember').show();
+        $('#box_slider .bxslider').bxSlider({
+            mode: 'fade',
+            auto: true,
+            captions: false,
+            control: false
+        });
+        $('#box_slideMember .bxslider').bxSlider({
+            slideWidth: 208,
+            minSlides: 1,
+            maxSlides: 4,
+            moveSlides: 1,
+            //auto: true,
+            autoControls: true
+        })
+        $('.module_post .module_content .item .extent').addClass('module_detail');
+
+
+        }
+    }
+}
+
+export default ReactWr
