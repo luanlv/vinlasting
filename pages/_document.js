380ef3b4b0dd9185f477a8ada53c77e4734622c4
Lưu Văn Luận <luanlv@users.noreply.github.com>
2018-07-24 13:43:20 +0700

    update

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,9 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
           <style>{`
             img.wp-smiley,
             img.emoji {
